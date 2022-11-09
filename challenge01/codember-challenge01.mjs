676482e88b11b270419f33c84837f6c21e1a2219
Diego Carrasco <dcarrask@gmail.com>
2022-11-09 19:32:39 +0100

    commit with first challenge

---
+++ challenge01/codember-challenge01.mjs
@@ -0,0 +1,31 @@
+// https://codember.dev/
+// Challenge01
+
+import { promises as fs } from 'fs';
+
+const run = async () => {
+
+  let data = await fs.readFile('./users.txt', 'utf8');
+
+  let result = data.replace(/\n\s*\n/g, '*****').replace(/\n/g, " ").split('*****');
+
+  let output = result.filter(user => {
+    return (
+      user.indexOf('fll:') != -1 &&
+      user.indexOf('eme:') != -1 &&
+      user.indexOf('usr:') != -1 &&
+      user.indexOf('psw:') != -1 &&
+      user.indexOf('age:') != -1 &&
+      user.indexOf('loc:') != -1);
+  });
+
+  let lastUser = output[output.length-1];
+
+  let email = lastUser.match(/(\usr:\S+\b)/ig)[0].split(':')[1];
+
+  console.log(`submit ${output.length}${email}`);
+
+}
+
+
+run();
